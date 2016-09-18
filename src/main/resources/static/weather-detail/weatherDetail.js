fb2979b5f7eecc1f23a441e7fdfac42c78d47dce
Shaun McGlinchey <shaunmcglinchey@users.noreply.github.com>
2016-09-18 20:42:26 -0300

    Add weather properties

--- src/main/resources/static/weather-detail/weatherDetail.js
+++ src/main/resources/static/weather-detail/weatherDetail.js
@@ -1,15 +1,19 @@
 angular.module('WeatherApp').component('weatherDetail', {
-        templateUrl: 'weather-detail/weatherDetail.html',
-        controller: function WeatherDetailController($scope, weatherService) {
-            $scope.greeting = "Hello Weather";
-            $scope.weather = weatherService.getWeather();
-            console.log('Weather:'+$scope.weather);
-        }
-    }).factory('weatherService', function($http){
+    templateUrl: 'weather-detail/weatherDetail.html',
+    controller: function WeatherDetailController($scope, weatherService) {
+        weatherService.getWeather()
+            .success(function (data) {
+                $scope.weather = data;
+                $scope.valid = true;
+            })
+            .error(function () {
+                $scope.valid = false;
+            });
+    }
+}).factory('weatherService', function ($http) {
     return {
-        getWeather: function() {
-            var weather = {description:"Rain"};
-            return weather;
+        getWeather: function () {
+            return $http.get('http://localhost:8080/weather/current/2643743');
         }
     };
 });
