0f4c6003938d046e30f9e4684e4842a87ff2e5b0
Shaun McGlinchey <shaunmcglinchey@users.noreply.github.com>
2016-09-19 18:59:57 -0300

    Add spec reporter, basic addition jasmine unit test

--- src/main/resources/static/app/weather-detail/weatherDetail.spec.js
+++ src/main/resources/static/app/weather-detail/weatherDetail.spec.js
@@ -1,5 +1,6 @@
-beforeEach(module('WeatherApp'));
-beforeEach(inject(function(weatherService, _$httpBackend_) {
-    service = weatherService;
-    $httpBackend = _$httpBackend_;
-}));
+describe('Weather factory', function() {
+    it('has a dummy spec to test 2 + 2', function() {
+        // An intentionally failing test. No code within expect() will never equal 4.
+        expect(2 + 2).toEqual(4);
+    });
+});
